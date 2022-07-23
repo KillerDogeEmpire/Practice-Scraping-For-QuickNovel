ce507d19444224ba794d7aaa5e11730e2bb7de7a
KillerDogeEmpire <edison.beckett@gmail.com>
2022-07-23 02:24:58 -0700

    start of my first scrape

--- app/src/main/java/com/lagradost/quicknovel/providers/NovelsOnlineProvider.kt
+++ app/src/main/java/com/lagradost/quicknovel/providers/NovelsOnlineProvider.kt
@@ -1,5 +1,6 @@
 package com.lagradost.quicknovel.providers
 
+import com.lagradost.quicknovel.HeadMainPageResponse
 import com.lagradost.quicknovel.MainAPI
 import com.lagradost.quicknovel.R
 
@@ -13,4 +14,53 @@ class NovelsOnlineProvider : MainAPI() {
 
     override val iconBackgroundId = R.color.white
 
+
+    override val tags = listOf(
+        Pair("All", ""),
+        Pair("Action", "action"),
+        Pair("Adventure", "adventure"),
+        Pair("Celebrity", "celebrity"),
+        Pair("Comedy", "comedy"),
+        Pair("Drama", "drama"),
+        Pair("Ecchi", "ecchi"),
+        Pair("Fantasy", "fantasy"),
+        Pair("Gender Bender", "gender-bender"),
+        Pair("Harem", "harem"),
+        Pair("Historical", "historical"),
+        Pair("Horror", "horror"),
+        Pair("Josei", "josei"),
+        Pair("Martial Arts", "martial-arts"),
+        Pair("Mature", "mature"),
+        Pair("Mecha", "mecha"),
+        Pair("Mystery", "mystery"),
+        Pair("Psychological", "psychological"),
+        Pair("Romance", "romance"),
+        Pair("School Life", "school-life"),
+        Pair("Sci-fi", "sci-fi"),
+        Pair("Seinen", "seinen"),
+        Pair("Shotacon", "shotacon"),
+        Pair("Shoujo", "shoujo"),
+        Pair("Shoujo Ai", "shoujo-ai"),
+        Pair("Shounen", "shounen"),
+        Pair("Shounen Ai", "shounen-ai"),
+        Pair("Slice Of Life", "slice-of-life"),
+        Pair("Sports", "sports"),
+        Pair("Supernatural", "supernatural"),
+        Pair("Tragedy", "tragedy"),
+        Pair("Wuxia", "wuxia"),
+        Pair("Xianxia", "xianxia"),
+        Pair("Xuanhaun", "xuanhuan"),
+        Pair("Yaoi", "yaoi"),
+        Pair("Yuri", "yuri"),
+    )
+
+    override suspend fun loadMainPage(
+        page: Int,
+        mainCategory: String?,
+        orderBy: String?,
+        tag: String?
+    ): HeadMainPageResponse {
+
+
+    }
 }
